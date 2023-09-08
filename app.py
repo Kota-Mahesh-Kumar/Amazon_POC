be423c6de45938f67c354bff5a92bdbf4a067c49
Kota-Mahesh-Kumar <72548938+Kota-Mahesh-Kumar@users.noreply.github.com>
2023-09-08 14:35:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,6 +174,30 @@ plots_by_area()
 
 st.write('')
 
+st.markdown(
+    """
+    <style>
+    .sidebar .sidebar-content {
+        flex-direction: column;
+    }
+    .block-container {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+<style>
+    .centered-header {
+        text-align: center;
+    }
+</style>
+""", unsafe_allow_html=True)
+st.markdown('<h1 class="centered-header">Data Table</h1>', unsafe_allow_html=True)
+
 dataframe_to_display = df_original.iloc[:,0:14]
 dataframe_to_display.rename(columns={'ASIN': 'ASIN', 'Length_mf': 'IR_Length', 'Breadth_mf' :'IR_Breadth', 'Height_mf' : 'IR_Height', 'Radius_mf' :'IR_Radius', 'Item Weight_mf': 'IR_Item_Weight', 'Net Quantity_mf': 'IR_Net_Quantity', 'Colour_mf' :'IR_Colour'}, inplace=True)
 
@@ -197,29 +221,7 @@ st.download_button(
     file_name="my_dataframe.csv",
     mime="text/csv"
 )
-# st.markdown(
-#     """
-#     <style>
-#     .sidebar .sidebar-content {
-#         flex-direction: column;
-#     }
-#     .block-container {
-#         display: flex;
-#         flex-direction: column-reverse;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
 
-# st.markdown("""
-# <style>
-#     .centered-header {
-#         text-align: center;
-#     }
-# </style>
-# """, unsafe_allow_html=True)
-# st.markdown('<h1 class="centered-header">Data Table</h1>', unsafe_allow_html=True)
 # def highlight_missing_data(s):
 #     """Highlight missing data in red."""
 #     missing = s.isna()
